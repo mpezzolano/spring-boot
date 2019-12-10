9c53852a87c4892491f52c393cf5cae6b061a921
marcelo <marcelo.pezzolano@globant.com>
2019-12-09 23:24:02 -0300

    Initial Commit

---
+++ src/main/java/com/hellokoding/springboot/view/HelloController.java
@@ -0,0 +1,22 @@
+package com.hellokoding.springboot.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping({"/", "/hello"})
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping({"/test", "/test"})
+    public String test(Model model, @RequestParam(value="name2", required=false, defaultValue="Worlddddd") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
